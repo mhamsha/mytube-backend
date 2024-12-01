9fab8e58fa5182b02a16a0776b914d316620eaa7
mhamsha <hamza.shahzad.8692@gmail.com>
2024-12-01 10:39:41 +0500

    Add Express middleware for CORS and cookie parsing; implement error handling and server startup logic

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express, { json } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    Credential: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
